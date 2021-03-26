384fef496173ef3d495b9e053b8b2d8ceebb70cd
Tiago Marciano <thiagomarciano@gmail.com>
2021-03-26 18:11:25 -0300

    Create ConversionFunctions.py

---
+++ assignPR/ConversionFunctions.py
@@ -0,0 +1,34 @@
+def characteristic(numString, c):
+    try:
+        c[0] = int(float(numString))
+        return True
+    except:
+        return False
+
+def mantissa(numString, numerator, denominator):
+    try:
+        aux = numString.split('.', 1)[1]
+        numerator[0] = int(aux)
+        cont = 1
+
+        for x in aux:
+            cont *= 10
+
+        denominator[0] = cont
+
+        return True
+    except:
+        return False
+
+number = '123.456'
+c = [0]
+n = [0]
+d = [0]
+
+if (characteristic(number, c) and mantissa(number, n, d)):
+    print(number)
+    print(c[0])
+    print(n[0])
+    print(d[0])
+else:
+    print('O número informado é inválido!')
