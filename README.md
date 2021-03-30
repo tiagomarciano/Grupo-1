a372e3689c7676f8019c0d0feccf35637cce56e2
Tiago Marciano <thiagomarciano@gmail.com>
2021-03-30 10:44:49 -0300

    Linha adicionada ao final do código, adicionado comentários e variáveis com nomes subjetivos

--- README.md
+++ README.md
@@ -8,6 +8,14 @@
 
 Pull request e Code review
 
+## Informações
+
+Exercício: Conversion Functions (F1)
+
+Linguagem: `Python 3.8.5`
+
+IDE: `PyCharm 2020.3`
+
 ## Grupo
 
 * Jordan Vinicius Guimarães 
--- assignPR/ConversionFunctions.py
+++ assignPR/ConversionFunctions.py
@@ -1,10 +1,25 @@
-def characteristic(numString, c):
+# A função characteristic atribui a parte inteira de uma string númerica
+# para a variável intNumber
+#
+# @param numString, string númerica que será convertida
+# @param intNUmber, recebe o valor inteiro da conversão da string númerica
+# @return True, se a conversão for bem sucedida e False, caso não consiga converter
+#
+def characteristic(numString, intNumber):
     try:
-        c[0] = int(float(numString))
+        intNumber[0] = int(float(numString))
         return True
     except:
         return False
 
+# A função mantissa atribui a parte decimal de uma string númerica a variável numerator,
+# e calcula o logartimo baseado no numerador
+#
+# @param numString, string númerica que será convertida
+# @param numerator, recebe um inteiro do valor decimal da conversão da string númerica
+# @param denominator, recebe o logartimo do numerator
+# @return True, se o calculo for bem sucedido e False, caso não consiga calcular
+#
 def mantissa(numString, numerator, denominator):
     try:
         aux = numString.split('.', 1)[1]
@@ -20,17 +35,18 @@ def mantissa(numString, numerator, denominator):
     except:
         return False
 
+# Método principal
 def main():
     number = '123.456'
-    c = [0]
-    n = [0]
-    d = [0]
+    intNumber = [0]
+    numerator = [0]
+    denomiator = [0]
 
-    if (characteristic(number, c) and mantissa(number, n, d)):
+    if (characteristic(number, intNumber) and mantissa(number, numerator, denomiator)):
         print(number)
-        print(c[0])
-        print(n[0])
-        print(d[0])
+        print(intNumber[0])
+        print(numerator[0])
+        print(denomiator[0])
     else:
         print('O número informado é inválido!')
 
